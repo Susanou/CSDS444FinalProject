976fff9e28fedb8a1a139f3e773c14415f819066
Fukurou <caos2804@gmail.com>
2021-11-17 21:46:22 -0500

    padding functions

--- tool.py
+++ tool.py
@@ -6,6 +6,16 @@ import pathlib
 
 from aes import decrypt, encrypt
 
+def padding(data):
+    length = 16 - (len(data) % 16)
+    data += bytes([length])*length
+    return data
+
+def unpad(data):
+    return data[:-data[-1]]
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="command line utility to encrypt and decrypt using different algorithms")
 
@@ -19,9 +29,10 @@ if __name__ == '__main__':
     group_aes.add_argument("-d", "--decrypt", action="store_true")
     parser_aes.add_argument("filename", type=str, help="File containing the message to encrypt/decrypt")
     parser_aes.add_argument("key", type=str, help="File containing the key used to encrypt/decrypt")
+    parser_aes.add_argument("-o", "--output", type=str, help="Output file", nargs='?')
 
     ## RSA PARSER
-    parse_rsa = subparsers.add_parser("rsa", help="RSA help")
+    parser_rsa = subparsers.add_parser("rsa", help="RSA help")
     group_rsa = parser_rsa.add_mutually_exclusive_group(required=True)
     group_rsa.add_argument("-e", "--encrypt", action="store_true")
     group_rsa.add_argument("-d", "--decrypt", action="store_true")
@@ -37,5 +48,18 @@ if __name__ == '__main__':
 
     if args.algo == "aes":
         print(args)
+        if args.encrypt:
+            with open(args.filename, "rb") as f:
+                plaintext = padding(f.read())
+            with open(args.key, "rb") as f:
+                key = padding(f.read())
+
+            if args.output != None:
+                with open(args.output, "wb") as f:
+                    f.write(encrypt(key, plaintext))
+            else:
+                with open(args.filename+".enc", "wb") as f:
+                    f.write(encrypt(key, plaintext))
+
     else:
         parser.print_help()
