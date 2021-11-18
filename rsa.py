822caccebb9f9cc934fc5b7a346076081adc48f3
Fukurou <caos2804@gmail.com>
2021-11-18 13:58:08 -0500

    RSA working + commented

--- rsa.py
+++ rsa.py
@@ -3,7 +3,7 @@ from Crypto.PublicKey import RSA # ONLY USED TO READ a .pem key file not used fo
 from Crypto.Hash import SHA1
 from Crypto.Signature.pss import MGF1
 from Crypto import Random
-from Crypto.Util.py3compat import bord
+from Crypto.Util.py3compat import bord, _copy_bytes
 
 from pwn import xor
 
@@ -19,40 +19,45 @@ def generate(bit_length=2048):
 
 def encrypt(m_file, k_file):
     with open(m_file, "rb") as f:
-        plaintext = f.read()
+        message = f.read()
     
     with open(k_file, "rb") as f:
         key = RSA.import_key(f.read())
     
 
     # RFC3447 to see the algorithm section 7.1.1
-    mod_bits = size(key.n)
-    k = ceil_div(mod_bits, 8) # from bits to bytes
-    hash_len = SHA1.digest_size
-    message_len = len(plaintext) 
+    modBits = size(key.n)
+    k = ceil_div(modBits, 8) # Convert from bits to bytes
+    hLen = SHA1.digest_size
+    mLen = len(message)
 
-    padding_len = k - message_len - 2*hash_len - 2
-    if padding_len < 0:
-        raise ValueError("Plaintext too long")
-
-    l_hash = SHA1.new(b'').digest()
-    ps = b'\x00' * padding_len
-    db = l_hash + ps + b'\x01' + plaintext
-
-    r_seed = Random.get_random_bytes(hash_len)
-
-    db_mask = MGF1(r_seed, k-hash_len-1, SHA1)
-
-    masked_db = xor(r_seed, db_mask)
-
-    seed_mask = MGF1(masked_db, hash_len, SHA1)
-
-    masked_seed = xor(r_seed, seed_mask)
-
-    encrypted_message = b'\x00' + masked_seed + masked_db
-    em_int = bytes_to_long(encrypted_message)
-    m_int = pow(em_int, key.e, key.n)
-
+    # Step 1b
+    ps_len = k - mLen - 2 * hLen - 2
+    if ps_len < 0:
+        raise ValueError("Plaintext is too long.")
+    # Step 2a
+    lHash = SHA1.new(b'').digest()
+    # Step 2b
+    ps = b'\x00' * ps_len
+    # Step 2c
+    db = lHash + ps + b'\x01' + _copy_bytes(None, None, message)
+    # Step 2d
+    ros = Random.get_random_bytes(hLen)
+    # Step 2e
+    dbMask = MGF1(ros, k-hLen-1, SHA1)
+    # Step 2f
+    maskedDB = xor(db, dbMask)
+    # Step 2g
+    seedMask = MGF1(maskedDB, hLen, SHA1)
+    # Step 2h
+    maskedSeed = xor(ros, seedMask)
+    # Step 2i
+    em = b'\x00' + maskedSeed + maskedDB
+    # Step 3a (OS2IP)
+    em_int = bytes_to_long(em)
+    # Step 3b (RSAEP)
+    m_int = int(pow(em_int, key.e, key.n))
+    # Step 3c (I2OSP)
     c = long_to_bytes(m_int, k)
     return c
 
@@ -63,42 +68,49 @@ def decrypt(m_file, k_file):
     with open(k_file, "rb") as f:
         key = RSA.import_key(f.read())
     
+    if not key.has_private():
+        raise TypeError("This is not a private key")
+
     # RFC3447 to see the algorithm section 7.1.2
-    mod_bits = size(key.n)
-    k = ceil_div(mod_bits, 8) # from bits to bytes
-    hash_len = SHA1.digest_size
+    modBits = size(key.n)
+    k = ceil_div(modBits,8) # Convert from bits to bytes
+    hLen = SHA1.digest_size
 
-    if len(ciphertext) != k or k < hash_len+2:
-        raise ValueError("Cipher text with incorrect length")
-    
-    ciphertext_int = bytes_to_long(ciphertext)
-
-    message_int = pow(ciphertext_int, key.d, key.n)
-    em = long_to_bytes(message_int, k)
-
-    l_hash = SHA1.new(b'').digest()
-
+    # Step 1b and 1c
+    if len(ciphertext) != k or k<hLen+2:
+        raise ValueError("Ciphertext with incorrect length.")
+    # Step 2a (O2SIP)
+    ct_int = bytes_to_long(ciphertext)
+    # Step 2b (RSADP)
+    m_int = int(pow(ct_int, key.d, key.n))
+    # Complete step 2c (I2OSP)
+    em = long_to_bytes(m_int, k)
+    # Step 3a
+    lHash = SHA1.new(b'').digest()
+    # Step 3b
     y = em[0]
-    masked_seed = em[1:hash_len+1]
-    masked_db = em[hash_len+1:]
-
-    seed_mask = MGF1(masked_db, hash_len, SHA1)
-    seed = xor(masked_seed, seed_mask)
-
-    db_mask = MGF1(seed, k-hash_len-1, SHA1)
-    db = xor(db_mask, masked_db)
-
-    one_b_pos = hash_len + db[hash_len:].find(b'\x01')
-    l_hash_1 = db[:hash_len]
-    invalid = bord(y) | int(one_b_pos < hash_len)
-    hash_comp = xor(l_hash_1, l_hash)
-
-    for x in hash_comp:
+    # y must be 0, but we MUST NOT check it here in order not to
+    # allow attacks like Manger's (http://dl.acm.org/citation.cfm?id=704143)
+    maskedSeed = em[1:hLen+1]
+    maskedDB = em[hLen+1:]
+    # Step 3c
+    seedMask = MGF1(maskedDB, hLen, SHA1)
+    # Step 3d
+    seed = xor(maskedSeed, seedMask)
+    # Step 3e
+    dbMask = MGF1(seed, k-hLen-1, SHA1)
+    # Step 3f
+    db = xor(maskedDB, dbMask)
+    # Step 3g
+    one_pos = hLen + db[hLen:].find(b'\x01')
+    lHash1 = db[:hLen]
+    invalid = bord(y) | int(one_pos < hLen)
+    hash_compare = xor(lHash1, lHash)
+    for x in hash_compare:
         invalid |= bord(x)
-    for x in db[hash_len:one_b_pos]:
+    for x in db[hLen:one_pos]:
         invalid |= bord(x)
-    
     if invalid != 0:
-        raise ValueError("Incorrect decryption")
-    
-    return db[one_b_pos+1:]
+        raise ValueError("Incorrect decryption.")
+    # Step 4
+    return db[one_pos + 1:]
