eca34aca71ae71e2b2dc9611149f374b99540905
Zhaokuan Chen <zxc347@case.edu>
2021-11-23 01:19:44 -0500

    Finished Implementing Caesar and Vigenere Cipher
    Implemented Caesar Encryption/Decryption
    Implemented Vigenere Encryption/Decryption
    Command to run Caesar:
    Encryption: python caeservig.py caesar -e plain.txt caesarkey.txt
    Decryption: python caeservig.py caesar -d plain.txt.enc caesarkey.txt
    Command to run Vigenere:
    Encryption: python caeservig.py vigenere -e vigenereplain.txt vigenerekey.txt
    Decryption: python caeservig.py vigenere -d vigenereplain.txt.enc vigenerekey.txt

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CSDS444FinalProject
 Repo to host our final crypto project for CSDS444 Computer Security class @ CWRU
+
+Command to run Caesar:
+Encryption: python caeservig.py caesar -e plain.txt caesarkey.txt
+Decryption: python caeservig.py caesar -d plain.txt.enc caesarkey.txt
+Command to run Vigenere:
+Encryption: python caeservig.py vigenere -e vigenereplain.txt vigenerekey.txt
+Decryption: python caeservig.py vigenere -d vigenereplain.txt.enc vigenerekey.txt
--- caeservig.py
+++ caeservig.py
@@ -1,7 +1,9 @@
 import argparse
 
-# python caeser.py caesar -e plain.txt caesarkey.txt
-# python caeser.py caesar -d plain.txt.enc caesarkey.txt
+# python caeservig.py caesar -e plain.txt caesarkey.txt
+# python caeservig.py caesar -d plain.txt.enc caesarkey.txt
+# python caeservig.py vigenere -e vigenereplain.txt vigenerekey.txt
+# python caeservig.py vigenere -d vigenereplain.txt.enc vigenerekey.txt
 def caesar_encrypt(text,s):
     encry_result = ""
    # transverse the plain text
@@ -52,6 +54,49 @@ def caesar_decrypt(text,s):
 #print ("Shift pattern : " + str(s))
 #print ("Plain text: " + caesar_decrypt(text,s))
 
+def generateKey(string, key):
+
+    return("" . join(key))
+
+def vigenere_encrpt(string, key):
+    cipher_text = []
+    key = list(key)
+    if len(string) == len(key):
+        key = key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    print (key)
+    for i in range(len(string)):
+        if string[i] == " ":
+            cipher_text.append(" ")
+        else:
+            x = (ord(string[i]) + ord(key[i])) % 26
+            x += ord('A')
+            cipher_text.append(chr(x))
+    encrypted_text = "" . join(cipher_text)
+    print ("Encrypted Text:" + encrypted_text)
+    return(encrypted_text)
+
+def vigenere_decrpt(string, key):
+    orig_text = []
+    key = list(key)
+    if len(string) == len(key):
+        key = key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    for i in range(len(string)):
+        if string[i] == " ":
+            orig_text.append(" ")
+        else:
+            x = (ord(string[i]) - ord(key[i]) + 26) % 26
+            x += ord('A')
+            orig_text.append(chr(x))
+    decrypted_text = "" . join(orig_text)
+    print ("Decrypted Text:" + decrypted_text)
+    return(decrypted_text)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="command line utility to encrypt and decrypt using different algorithms")
 
@@ -66,6 +111,15 @@ if __name__ == '__main__':
     parser_caesar.add_argument("filename", type=str, help="File containing the message to encrypt/decrypt")
     parser_caesar.add_argument("key", type=str, help="File containing the key used to encrypt/decrypt")
     parser_caesar.add_argument("-o", "--output", type=str, help="Output file", nargs='?')
+    
+    parser_vigenere = subparsers.add_parser("vigenere", help="vigenere help")
+    group_vigenere = parser_vigenere.add_mutually_exclusive_group(required=True)
+    group_vigenere.add_argument("-e", "--encrypt", action="store_true")
+    group_vigenere.add_argument("-d", "--decrypt", action="store_true")
+    parser_vigenere.add_argument("-c", "--create", action="store_true")
+    parser_vigenere.add_argument("filename", type=str, help="File containing the message to encrypt/decrypt")
+    parser_vigenere.add_argument("key", type=str, help="File containing the key used to encrypt/decrypt", nargs="?", default="private.pem")
+    parser_vigenere.add_argument("-o", "--output", type=str, help="Output file", nargs='?')
 
     args = parser.parse_args()
 
@@ -95,5 +149,31 @@ if __name__ == '__main__':
                     f.write(caesar_decrypt(ciphertext, key))
         else:
             parser.print_help()
+    elif args.algo == "vigenere":
+        if args.encrypt:
+            with open(args.filename, "r") as f:
+                plaintext = f.read()
+            with open(args.key, "r") as f:
+                key = f.read()
+            if args.output != None:
+                with open(args.output, "w") as f:
+                    f.write(vigenere_encrpt(plaintext, key))
+            else:
+                with open(args.filename+".enc", "w") as f:
+                    f.write(vigenere_encrpt(plaintext, key))
+        elif args.decrypt:
+            with open(args.filename, "r") as f:
+                ciphertext = f.read()
+            with open(args.key, "r") as f:
+                key = f.read()
+
+            if args.output != None:
+                with open(args.output, "w") as f:
+                    f.write(vigenere_decrpt(ciphertext, key))
+            else:
+                with open(".".join(args.filename.split('.')[:2]), "w") as f:
+                    f.write(vigenere_decrpt(ciphertext, key))
+        else:
+            parser.print_help()    
     else:
         parser.print_help()
