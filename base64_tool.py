67962d2c6deeec1e670b4e322348c45f1dca8614
mss231 <93743321+mss231@users.noreply.github.com>
2021-12-01 23:07:29 -0500

    fixed bug

--- base64_tool.py
+++ base64_tool.py
@@ -1,62 +1,44 @@
+lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+
 def encrypt(message):
+    cipher_text = ''
+    unibits = ''
+    for char in message:
+        bin_char = bin(ord(char)).lstrip('0b')
+        bin_char = bin_char.zfill(8)
+        unibits += bin_char
 
-    # get unicode values for characters
-    output = ''.join(bin(ord(char)) for char in message)
-    # remove b's. There's probably a better way to do this...
-    output = output.replace("b", "")
-    #print('Binary Unicode of Output:', output)
+    groups = [unibits[x:x+6] for x in range(0, len(unibits), 6)]
 
-    # make length multiple of 6
-    if len(output) < 6:
-        output = output + ('0'*(6-len(output)))
-    else:
-        output = output + ('0'*(len(output) % 6))
-    #print('Binary Unicode of Output with extra 0s:', output)
+    if len(groups[-1]) < 6:
+        groups[-1] += ('0'*(6-len(groups[-1][:])))
 
-    lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    for group in groups:
+        cipher_text += lookup[int(group, 2)]
 
-    cipher = ''
-    # Get values for cipher from lookup table
-    for x in range(1, int(len(output)/6 + 1)):
-        cipher = cipher + lookup[int(output[(x - 1) * 6:x * 6], 2)]
-    # append '=' at end of cipher to make multiple of 4
-    if len(cipher) < 4:
-        cipher = cipher + ('='*(4-len(cipher)))
-    else:
-        cipher = cipher + ('='*(len(cipher) % 4))
-    #print("Cipher Text:", cipher)
-    return cipher
+    while (len(cipher_text) ) % 4 != 0:
+        cipher_text = cipher_text + '='
+
+    return cipher_text
 
 
-def decrypt(encodedMessage):
+def decrypt(cipher_text):
+    bit_str = ''
+    decoded_text = ''
 
-    encodedMessage = encodedMessage.replace('=', '')
-    encodedMessage = encodedMessage.replace(' ', '')
+    for char in cipher_text:
+        if char in lookup:
+            bin_char = bin(lookup.find(char)).lstrip('0b')
+            bin_char = bin_char.zfill(6)
+            bit_str += bin_char
 
-    lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    #print('Encoded Message:', encodedMessage)
+    while len(bit_str) % 8 != 0:
+        bit_str += '0'
 
-    decoded_bin = ''
-    # get binary values for table lookups of characters
-    for char in encodedMessage:
-        #num = lookup.find(char)
-        messageBin = (bin(lookup.find(char))).replace('b', '')
+    groups = [bit_str[i:i+8] for i in range(0, len(bit_str), 8)]
 
-        if len(messageBin) == 7:
-            messageBin = messageBin[1:]
-        elif len(messageBin) < 6:  # if length is less than 6, add 0s to the beginning
-            messageBin = '0'*(6-len(messageBin)) + messageBin
-        decoded_bin += messageBin
-    # make multiple of 8 for unicode printing
-    while len(decoded_bin) % 8 != 0:
-        decoded_bin += '0'
-    #print(decoded_bin)
-    decoded_message = ''
-    # create decoded message
-    for x in range(1, int(len(decoded_bin) / 8 + 1)):
-        decoded_message = decoded_message + chr(int(decoded_bin[(x - 1) * 8:x * 8], 2))
+    for group in groups:
+        decoded_text += chr(int(group, 2))
 
-    return decoded_message
-
-#print(encrypt('MessageToBeEncoded'))
-#print(decrypt('TWVzc2FnZVRvQmVFbmNvZGVk'))
+    return decoded_text
